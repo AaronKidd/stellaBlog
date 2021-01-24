4ffe74b1d2f3907cacd2ac43028f6ec7b1d665a5
AaronKidd <64435877+AaronKidd@users.noreply.github.com>
2021-01-24 18:50:52 +0000

    css for all pages on desktop complete, blogs can now be posted locally

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const _ = require('lodash');
+
+const {
+  forEach
+} = require("lodash");
+
+
 
 app.set('view engine', 'ejs');
 
@@ -10,6 +17,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+const posts =[]
 
 
 
@@ -21,8 +29,62 @@ app.get("/", function (req, res) {
 
 
 app.get("/blog", function (req, res) {
-  res.render("blog", {});
+  res.render("blog", {
+    posts: posts
+  });
 });
+
+
+
+
+//blog posts
+app.get("/compose", function (req, res) {
+  res.render("compose", {});
+});
+
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+
+today = dd+'/'+mm+'/'+yyyy;
+
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    dateposted: today,
+    post: req.body.postBody
+  }
+  posts.push(post)
+  res.redirect("/blog")
+});
+
+
+app.get('/posts/:postname', (req, res) => {
+  const searchedTitle = req.params.postname
+  posts.forEach(post => {
+    if (_.lowerCase(post.title) === _.lowerCase(searchedTitle)) {
+      res.render('post', {
+        title: post.title,
+        dateposted: post.dateposted,
+        post: post.post
+      });
+    
+    }
+  });
+    
+})
+
 
 app.get("/contact", function (req, res) {
   res.render("contact", {});
