3f6969dbbcf12c861b98651cb0da220fe5423414
AaronKidd <64435877+AaronKidd@users.noreply.github.com>
2021-01-25 23:29:34 +0000

    added files

--- index.js
+++ index.js
@@ -119,12 +119,15 @@ app.get("/about", function (req, res) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000
+}
 
 
 
 
 
-
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen( port, function () {
+  console.log("Server started");
 });
